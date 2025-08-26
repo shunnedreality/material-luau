6ef068712ecb8ac30289e28fb2884c0b24aaaf04
shunnedreality <vrooms.get.8p@icloud.com>
2025-08-26 21:51:24 +0000

    feat: create new mkdocs

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,6 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.mkdocs
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
